ad32fa8622e16d0b8feed914e17308616867cecd
Md. Abdul Alim <92217313+AbdulAlimRony@users.noreply.github.com>
2021-12-08 10:08:53 +0600

    Update home.php

--- home.php
+++ home.php
@@ -1,1 +1,23 @@
-echo "hi";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<pre>";
+    echo $_POST['cardNo'];
+    echo $_POST['id'];
+    echo $_POST['session'];
+    echo $_POST['city'];
+    echo $_POST['fullName'];
+    echo $_POST['userName'];
+    echo $_POST['contactNo'];
+    echo $_POST['email'];
+    echo "</pre>";
+    ?>
+</body>
+</html>
